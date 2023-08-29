599238cb53dd0a54e76b033b37618b3103f01648
Nguyễn Đức Quân <68120446+NguyenDucQuan12@users.noreply.github.com>
2023-08-29 09:30:21 +0700

    Lỗi khi xác địng ngưỡng bất thường không hợp lý

---
+++ save_and_load_models.py
@@ -0,0 +1,182 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import transforms
+import os
+import shutil
+import datetime
+
+number_epoch = 3
+number_epoch_train = 80
+load_model = True
+
+
+def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
+    print("Save checkpoint")
+    torch.save(state, filename)
+
+
+def load_checkpoint(checkpoint):
+    print("loading checkpoint")
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),  # Unify images to (224,224) size
+    transforms.ToTensor()
+])
+
+train_dir = './screw/train'
+
+test_dir = './screw/test'
+
+train_dataset = ImageFolder(root=train_dir, transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+
+test_dataset = ImageFolder(root=test_dir, transform=transform)
+
+test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
+
+class_names = test_dataset.classes
+print(class_names)
+
+
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(16, 8, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(8, 4, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(4, 8, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(8, 16, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 3, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+model = Autoencoder().to(device)
+
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)  # là tốc độ học (learning rate)
+
+if load_model:
+    load_checkpoint(torch.load("my_checkpoint.pth.tar"))
+
+for epoch in range(number_epoch):
+    running_loss = 0.0
+    if epoch % 3 == 0:
+        checkpoint = {'state_dict': model.state_dict(), 'optimizer': optimizer.state_dict()}
+        save_checkpoint(checkpoint)
+    for data in train_loader:
+        img, _ = data
+        img = img.to(device)
+        optimizer.zero_grad()
+        outputs = model(img)
+
+        loss = criterion(outputs, img)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+
+    print('Epoch [%d], Loss: %.4f' % (epoch + 1, running_loss / len(train_loader)))
+
+model.eval()
+test_loss = 0.0
+with torch.no_grad():
+    for data in test_loader:
+        img, _ = data
+        img = img.to(device)
+        outputs = model(img)
+        loss = criterion(outputs, img)
+        test_loss += loss.item()
+
+print('Test Loss: %.4f' % (test_loss / len(test_loader)))
+
+model.eval()
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+class_encodings = [[] for i in range(6)]
+with torch.no_grad():
+    for data in test_loader:
+        img, label = data
+        img = img.to(device)
+        encoding = model.encoder(img)
+        class_encodings[label.item()].append(encoding.cpu().numpy().ravel())
+
+
+def detect_and_save_anomalies(model, test_loader, threshold, save_dir_anomaly, save_dir_normal):
+    os.makedirs(save_dir_normal, exist_ok=True)
+    os.makedirs(save_dir_anomaly, exist_ok=True)
+    model.eval()
+
+    with torch.no_grad():
+        for i, data in enumerate(test_loader):
+            img, _ = data
+            img = img.to(device)
+            outputs = model(img)
+            loss = criterion(outputs, img)
+
+            if loss.item() > threshold:
+                img_filename = test_dataset.samples[i][0]
+                img_basename = os.path.basename(img_filename)
+                destination_path = os.path.join(save_dir_anomaly, img_basename)
+                shutil.copy(img_filename, destination_path)
+                print("anomaly"+str(i) + ": "+img_filename)
+
+            else:
+                img_filename = test_dataset.samples[i][0]
+                img_basename = os.path.basename(img_filename)
+                destination_path = os.path.join(save_dir_normal, img_basename)
+                shutil.copy(img_filename, destination_path)
+                print("normal"+str(i) + ": "+img_filename)
+
+
+# Sử dụng hàm để phát hiện và lưu ảnh có bất thường
+# Điều chỉnh ngưỡng bất thường dựa trên độ chính xác của mô hình và kết quả mỗi lần điều chỉnh
+# anomaly_threshold = (test_loss / len(test_loader))
+# Điều chỉnh lần 1 tất cả mô hình đều normal
+# anomaly_threshold = 0.01
+# Điều chỉnh ngưỡng lần 2 tất cả đều anomaly
+# anomaly_threshold = 0.001
+# Điều chỉnh lần 3 tất cả đều normal, lần tiếp theo sẽ nằm giữa 0,001 và 0,005
+# anomaly_threshold = 0.005
+# Điều chỉnh lần 4 tất cả đều normal
+# anomaly_threshold = 0.0025
+# Điều chỉnh lần 5 cho ra kết quả lộn xộn, không chính xác
+# anomaly_threshold = 0.0015
+# Điều chỉnh lần 6 tất cả đều anomaly
+# anomaly_threshold = 0.0012
+# Điều chỉnh lần 7 tất cả đều anomaly
+# anomaly_threshold = 0.0013
+# Điều chỉnh lần 8 kết quả lộn xộn, không chính xác
+anomaly_threshold = 0.0014
+now = datetime.datetime.now()
+today = str(now.strftime("%d-%m-%y %Hh%Mp%Ss"))
+name_folder_anomaly = "anomaly_images"
+name_folder_anomaly = today + " " + name_folder_anomaly
+
+name_folder_normal = "normal_images"
+name_folder_normal = today + " " + name_folder_normal
+detect_and_save_anomalies(model, test_loader, anomaly_threshold, name_folder_anomaly, name_folder_normal)
+print("Phát hiện và lưu ảnh có bất thường hoàn thành.")
